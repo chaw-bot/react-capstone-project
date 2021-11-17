4a1e068e08e3ee7efeff56e5b8c105242fe3eb43
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-17 10:18:06 +0200

    Create dummy data

---
+++ src/components/data.js
@@ -0,0 +1,130 @@
+const data = [
+  {
+    id: 1,
+    country: 'Afghanistan',
+    population: 1382093,
+    today: {
+      confirmed: 0,
+      deaths: 0,
+    },
+    latest: {
+      confirmed: 78,
+      recovered: 56,
+      critical: 5,
+      deaths: 54,
+    },
+    countryStats: {
+      deathRate: 5,
+      recoveryRate: 7,
+      recover_deathRatio: 64,
+      casePerMilli: 422,
+    },
+  },
+  {
+    id: 2,
+    country: 'Zambia',
+    population: 13820,
+    today: {
+      confirmed: 4,
+      deaths: 56,
+    },
+    latest: {
+      confirmed: 7,
+      recovered: 43,
+      critical: 32,
+      deaths: 43,
+    },
+    countryStats: {
+      deathRate: 2,
+      recoveryRate: 21,
+      recover_deathRatio: 1,
+      casePerMilli: 3332,
+    },
+  },
+  {
+    id: 3,
+    country: 'Angola',
+    population: 13893,
+    today: {
+      confirmed: 42,
+      deaths: 431,
+    },
+    latest: {
+      confirmed: 42,
+      recovered: 423,
+      critical: 53,
+      deaths: 63,
+    },
+    countryStats: {
+      deathRate: 543,
+      recoveryRate: 34,
+      recover_deathRatio: 4,
+      casePerMilli: 4,
+    },
+  },
+  {
+    id: 4,
+    country: 'South Africa',
+    population: 1382092,
+    today: {
+      confirmed: 4,
+      deaths: 4,
+    },
+    latest: {
+      confirmed: 4,
+      recovered: 4,
+      critical: 54,
+      deaths: 6,
+    },
+    countryStats: {
+      deathRate: 643,
+      recoveryRate: 3,
+      recover_deathRatio: 6,
+      casePerMilli: 6,
+    },
+  },
+  {
+    id: 5,
+    country: 'Austria',
+    population: 1382,
+    today: {
+      confirmed: 6,
+      deaths: 6,
+    },
+    latest: {
+      confirmed: 56,
+      recovered: 6,
+      critical: 6,
+      deaths: 0,
+    },
+    countryStats: {
+      deathRate: 5,
+      recoveryRate: 6,
+      recover_deathRatio: 5,
+      casePerMilli: 4,
+    },
+  },
+  {
+    id: 6,
+    country: 'Morocco',
+    population: 1382,
+    today: {
+      confirmed: 6,
+      deaths: 6,
+    },
+    latest: {
+      confirmed: 56,
+      recovered: 6,
+      critical: 6,
+      deaths: 0,
+    },
+    countryStats: {
+      deathRate: 5,
+      recoveryRate: 6,
+      recover_deathRatio: 5,
+      casePerMilli: 4,
+    },
+  },
+];
+
+export default data;
