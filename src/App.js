ccdb22f81087112a80686dcee1fa0f9773071b0a
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-27 00:11:25 +0200

    Add more components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import HomePage from './pages/HomePage';
+import ContinentPage from './pages/ContinentPage';
 import Country from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
 
 const App = () => (
   <Router>
     <NavBar />
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route path="/continent/:continent" element={<ContinentPage />} />
       <Route path="/country/:name" element={<Country />} />
     </Routes>
   </Router>
